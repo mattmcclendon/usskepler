34726440d0d26971f12a48f09dfc4d78ccdea68d
Matthew McClendon <matt.mcclendon@gmail.com>
2026-07-16 09:44:38 -0700

    Stabilize Kepler discovery and search

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://www.usskepler.org',
+  integrations: [
+    sitemap({
+      filter: (page) => page !== 'https://www.usskepler.org/search/',
+    }),
+  ],
+});
